41b0bc71a82e02fce29cbaccc48f7afd8abe70e5
Suriya-K7 <usraising@gmail.com>
2023-05-02 14:11:45 +0530

    modifing javascript as per task

--- JS/day7Task.js
+++ JS/day7Task.js
@@ -4,27 +4,23 @@ const req = new XMLHttpRequest();
       req.addEventListener("load", request);
       function request() {
         let data = JSON.parse(this.responseText);
+        // log - asian countries
         let asianCountries = data.filter((e) => {
           if (e.continents[0] == "Asia") {
             return e;
           }
         });
+        console.log("All asian countries filter & logged below");
+        console.log(asianCountries);
+        // log - countries with less than 2 lakhs population
         let Population = data.filter((e) => {
           if (e.population < 200000) {
             return e;
           }
         });
-        let totalPopulation = data
-          .map((e) => {
-            return e.population;
-          })
-          .reduce((acc, cur) => (acc += cur));
-        console.log("All asian countries filter & logged below");
-        console.log(asianCountries);
-        console.log(
-          "All countries with a population of less than 2 lakhs Filtered & logged below"
-        );
+        console.log("All countries with a population of less than 2 lakhs Filtered & logged below");
         console.log(Population);
+        // log - all countries name, capital & flag using forEach
         console.log("All countries name, capital & flag logged below");
         data.forEach((e) => {
           console.log(`
@@ -34,8 +30,15 @@ Country flag : ${e.flag}
 
 `);
         });
+        // log - total population using reduse method
+        let totalPopulation = data
+          .map((e) => {
+            return e.population;
+          })
+          .reduce((acc, cur) => (acc += cur));
         console.log("total population logged below using reduse :");
         console.log(totalPopulation);
+        // log - countries using USD as currency
         console.log("countries using USD logged below :");
         let filteredCountryWithCurrencies=data.filter((e) => {
             if(e.currencies!=undefined){
